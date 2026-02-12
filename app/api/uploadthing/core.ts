3c2057eaf8e72b15fa4c7b3f96de61b81d22e0b6
Bhuvan-S-prasad <bhuvansprasad20@gmail.com>
2026-02-12 15:46:00 +0530

    feat: initialize uploadthing for file and image uploading, - add uploadthing core and api route - change global css with tweakcn theme - add shadcn components for form - create channel setup form wth image upload

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,29 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { auth } from "@clerk/nextjs/server";
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+    const userId = auth();
+    if(!userId) throw new Error("Unauthorized");
+    return { userId };
+}
+
+
+export const ourFileRouter = {
+  serverImage: f({ image: {maxFileSize: "4MB", maxFileCount: 1} })
+    .middleware(() => handleAuth())
+    .onUploadComplete((data) => {
+        console.log(data);
+    }),
+
+    messageFile: f(["image", "pdf" ])
+    .middleware(() => handleAuth())
+    .onUploadComplete((data) => {
+        console.log(data);
+    })
+        
+    
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
