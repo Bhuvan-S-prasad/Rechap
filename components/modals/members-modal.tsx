ab2ac74c9ae36d18c5f7030700e7318d7ea79bab
Bhuvan-S-prasad <bhuvansprasad20@gmail.com>
2026-02-17 09:19:46 +0530

    feat: add MembersModal component for viewing and managing channel members and their roles.

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -9,21 +9,45 @@ import {
 } from "@/components/ui/dialog";
 
 import { useModal } from "@/hooks/use-modal-store";
-import { ShieldAlert, ShieldCheck } from "lucide-react";
+import {
+  Check,
+  Loader2,
+  MoreVertical,
+  ShieldAlert,
+  ShieldCheck,
+  ShieldQuestion,
+  Trash,
+} from "lucide-react";
 import { ChannelWithMembersWithProfiles } from "@/types";
 import { ScrollArea } from "../ui/scroll-area";
 import { UserAvatar } from "../user-avatar";
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
-const roleIcons = {
+const roleIcons: Record<string, React.ReactNode> = {
   ADMIN: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
   MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-emerald-500" />,
-  GUEST: "",
+  GUEST: null,
 };
 
 export const MembersModal = () => {
-  const { isOpen, onClose, type, data, onOpen } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
+
   const isModalOpen = isOpen && type === "members";
-  const { channel } = data as { channel: ChannelWithMembersWithProfiles };
+
+  const channel = (data as { channel?: ChannelWithMembersWithProfiles })
+    ?.channel;
+
   const [loadingId, setLoadingId] = useState("");
 
   return (
@@ -34,24 +58,71 @@ export const MembersModal = () => {
             Manage Members
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500 ">
-            {channel?.members.length} Members
+            {channel?.members.length ?? 0} Members
           </DialogDescription>
         </DialogHeader>
+
         <ScrollArea className="mt-8 max-h-[420px] pr-6">
-          {channel?.members.map((member) => (
-            <div key={member.id} className="flex items-center gap-x-2 mb-4">
-              <UserAvatar src={member.user.imageUrl} />
-              <div className="flex flex-col gap-y-1">
-                <div className="text-xs font-semibold flex items-center gap-x-1">
-                  {member.user.name}
-                  {roleIcons[member.role]}
+          {channel?.members.map(
+            (member: ChannelWithMembersWithProfiles["members"][number]) => (
+              <div key={member.id} className="flex items-center gap-x-2 mb-4">
+                <UserAvatar src={member.user.imageUrl} />
+                <div className="flex flex-col gap-y-1">
+                  <div className="text-xs font-semibold flex items-center gap-x-1">
+                    {member.user.name}
+                    {roleIcons[member.role]}
+                  </div>
+                  <p className="text-xs text-muted-foreground">
+                    {member.user.email}
+                  </p>
                 </div>
-                <p className="text-xs text-muted-foreground">
-                  {member.user.email}
-                </p>
+                {channel.userId !== member.userId &&
+                  loadingId !== member.id && (
+                    <div className="ml-auto">
+                      <DropdownMenu>
+                        <DropdownMenuTrigger>
+                          <MoreVertical className="h-4 w-4" />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent side="left">
+                          <DropdownMenuSub>
+                            <DropdownMenuSubTrigger className="flex items-center">
+                              <ShieldQuestion className="h-4 w-4 mr-2" />
+                              <span>Role</span>
+                            </DropdownMenuSubTrigger>
+                            <DropdownMenuPortal>
+                              <DropdownMenuSubContent>
+                                <DropdownMenuItem>
+                                  <ShieldQuestion className="h-4 w-4 mr-2" />
+                                  <span>Guest</span>
+                                  {member.role === "GUEST" && (
+                                    <Check className="h-4 w-4 ml-auto" />
+                                  )}
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                  <ShieldCheck className="h-4 w-4 mr-2" />
+                                  <span>Moderator</span>
+                                  {member.role === "MODERATOR" && (
+                                    <Check className="h-4 w-4 ml-auto" />
+                                  )}
+                                </DropdownMenuItem>
+                              </DropdownMenuSubContent>
+                            </DropdownMenuPortal>
+                          </DropdownMenuSub>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem>
+                            <Trash className="h-4 w-4 mr-2" />
+                            Remove Member
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </div>
+                  )}
+                {loadingId === member.id && (
+                  <Loader2 className="h-4 w-4 ml-auto animate-spin" />
+                )}
               </div>
-            </div>
-          ))}
+            ),
+          )}
         </ScrollArea>
       </DialogContent>
     </Dialog>
