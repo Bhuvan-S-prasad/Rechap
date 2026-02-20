8383fe84dfd4b0456132bdcc91b8086b3e93b20f
Bhuvan-S-prasad <bhuvansprasad20@gmail.com>
2026-02-20 09:13:28 +0530

    feat(search): add search bar to the channel sidebar with commandDialog component

--- components/channel/channel-sidebar.tsx
+++ components/channel/channel-sidebar.tsx
@@ -2,10 +2,25 @@ import { currentUser } from "@/lib/current-user";
 import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import { ChannelHeader } from "./channel-header";
+import { ScrollArea } from "../ui/scroll-area";
+import { ChannelSearch } from "./channel-search";
+import { Hash, Mic, ShieldAlert, ShieldCheck, User, Video } from "lucide-react";
 
 interface ChannelSidebarProps {
   channelId: string;
 }
+
+const iconMap = {
+  TEXT: <Hash className="w-4 h-4" />,
+  AUDIO: <Mic className="w-4 h-4" />,
+  VIDEO: <Video className="w-4 h-4" />,
+};
+
+const roleIconMap = {
+  ADMIN: <ShieldAlert className="w-4 h-4 mr-1 text-rose-500" />,
+  MODERATOR: <ShieldCheck className="w-4 h-4 mr-1 text-emerald-500" />,
+  GUEST: <User className="w-4 h-4 mr-1 text-zinc-500" />,
+};
 
 export const ChannelSidebar = async ({ channelId }: ChannelSidebarProps) => {
   const profile = await currentUser();
@@ -52,6 +67,50 @@ export const ChannelSidebar = async ({ channelId }: ChannelSidebarProps) => {
   return (
     <div className="flex flex-col h-full text-primary w-full p-3 dark:bg-background border-t-[0.2px] border-l-[0.2px] border-zinc-500 rounded-tl-2xl">
       <ChannelHeader channel={channel} role={role} />
+      <ScrollArea className="flex-1 overflow-y-auto">
+        <div className="mt-3">
+          <ChannelSearch
+            data={[
+              {
+                label: "Text Room",
+                type: "room",
+                data: textRooms.map((room) => ({
+                  icon: iconMap[room.type],
+                  id: room.id,
+                  name: room.name,
+                })),
+              },
+              {
+                label: "Voice room",
+                type: "room",
+                data: voiceRooms.map((room) => ({
+                  icon: iconMap[room.type],
+                  id: room.id,
+                  name: room.name,
+                })),
+              },
+              {
+                label: "Video room",
+                type: "room",
+                data: videoRooms.map((room) => ({
+                  icon: iconMap[room.type],
+                  id: room.id,
+                  name: room.name,
+                })),
+              },
+              {
+                label: "Members",
+                type: "member",
+                data: members.map((member) => ({
+                  icon: roleIconMap[member.role],
+                  id: member.id,
+                  name: member.user.name,
+                })),
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 };
